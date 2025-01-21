6a3704a35aa5c70c62898d8e11a27d7a784ecd48
ishiiii10 <166246217+ishiiii10@users.noreply.github.com>
2025-01-21 17:29:16 +0530

    Controllers Done

--- Backend/controllers/taskController.js
+++ Backend/controllers/taskController.js
@@ -1,28 +1,33 @@
 const Task = require('../models/Task');
 
-const getTasks = async (req, res) => {
-  const tasks = await Task.find({ userId: req.user.id });
-  res.json(tasks);
+//Get Task
+const getTask = async(req, res)=>{
+    const tasks= await Task.find({ userId: req.user.id});
+    res.json(tasks);
 };
 
-const addTask = async (req, res) => {
-  const { title, description } = req.body;
-  const task = await Task.create({ userId: req.user.id, title, description });
-  res.status(201).json(task);
+//Add Task
+const addTask = async(req, res)=>{
+    const { title, description } = req.body;
+    const task= await Task.create({userId: req.user.id, title, description});
+    res.status(201).json(task);
 };
 
-const updateTask = async (req, res) => {
-  const task = await Task.findById(req.params.id);
-  if (task.userId.toString() !== req.user.id) return res.status(401).json({ message: 'Unauthorized' });
-  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(updatedTask);
+//Update Task
+
+const updateTask = async (req, res)=>{
+    const task= await Task.findById(req.params.id);
+    if(task.userId.toString() !== req.user.id) return res.status(401).json({ message: 'Unauthorized'});
+    const updatedTask=await Task.findByIdAndUpdate(req.params.id, req.body, { new: true});
+    res.json(updatedTask);
+}
+
+//Delete Task
+const deleteTask= async(req, res) => {
+    const task= await task.findById(req.params.id);
+    if(task.userId.toString() != req.user.id) return res.status(401).json({message: 'Unauthorized'});
+    await task.remove();
+    res.json({message: 'Task Removed'});
 };
 
-const deleteTask = async (req, res) => {
-  const task = await Task.findById(req.params.id);
-  if (task.userId.toString() !== req.user.id) return res.status(401).json({ message: 'Unauthorized' });
-  await task.remove();
-  res.json({ message: 'Task removed' });
-};
-
-module.exports = { getTasks, addTask, updateTask, deleteTask };
+module.exports = { getTask, addTask, updateTask, deleteTask };
