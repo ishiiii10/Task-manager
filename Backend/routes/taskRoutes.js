62a97a1e8437f0ac6de2e9fd90339b96c8f9e2f8
ishiiii10 <166246217+ishiiii10@users.noreply.github.com>
2025-01-23 12:14:57 +0530

    backend done

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { getTasks, addTask, updateTask, deleteTask } = require('../controllers/taskController');
+const { getTasks, addTask, updateTask, deleteTask, getTask } = require('../controllers/taskController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.get('/', protect, getTasks);
+router.get('/', protect, getTask);
 router.post('/', protect, addTask);
 router.put('/:id', protect, updateTask);
 router.delete('/:id', protect, deleteTask);
