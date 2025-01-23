2009ce05188fc63e1d4984139d980329b5fd6ef0
ishiiii10 <166246217+ishiiii10@users.noreply.github.com>
2025-01-23 11:45:53 +0530

    middleware completed

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,20 +1,20 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User= require('../models/User');
 
-const protect = async (req, res, next) => {
-  let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select('-password');
-      next();
-    } catch (err) {
-      res.status(401).json({ message: 'Unauthorized' });
+const protect= async (req, res, next)=>{
+    let token;
+    if(req.headrer.authorization && req.header.authorization.startsWith('Bearer')){
+        try{
+            token= req.header.authorization.spilt('')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user=await User.findById(decoded.id).select('-password');
+            next();
+        } catch(err){
+            res.status(402).json({message: 'Unauthorized'});
+        }
+    }else{
+        res.status(401).json({message: 'No Token Provided'});
     }
-  } else {
-    res.status(401).json({ message: 'No token provided' });
-  }
 };
 
-module.exports = { protect };
+module.exports = {protect}; 
